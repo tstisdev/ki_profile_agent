29989822dda3b2d98320cce2dc0b73e98b9c25d4
Khalil <khalil.knifati@biqx.de>
2025-08-21 15:42:17 +0200

    spacy

--- TestProject/main.py
+++ TestProject/main.py
@@ -1,38 +1,60 @@
-import requests
+import pdfplumber
+import spacy
 from pathlib import Path
-from fpdf import FPDF
-from PyPDF2 import PdfReader
+from reportlab.pdfgen import canvas
+from io import BytesIO
 
-
-OUTPUT_DIR = Path(__file__).parent/"generated_pdfs"
+INPUT_DIR = Path(__file__).parent/"Data"
+OUTPUT_DIR = Path(__file__).parent/"Anonymized"
 OUTPUT_DIR.mkdir(exist_ok=True)
 
+try:
+    nlp = spacy.load("de_core_news_lg")
+except ModuleNotFoundError:
+    print("spaCy package 'de_core_news_lg' not found.")
 
-def create_pdfs():
-    for i in range(0, 30):
-        data = requests.get(f"https://jsonplaceholder.typicode.com/posts/{i+1}").json()
-        title = data.get("title").strip().capitalize()
-        body = data.get("body").strip()
-        filename = f"{i+1:02d} - {title}.pdf"
+def anonymize(text: str, per_id: int) -> str:
+    doc = nlp(text)
+    anonymized_text = text
+    for ent in reversed(doc.ents):
+        if ent.label_ in {"PER"}:
+            anonymized_text = (
+                anonymized_text[:ent.start_char] + f"[{ent.label_}]" + anonymized_text[ent.end_char:]
+            )
+    return anonymized_text
 
-        pdf = FPDF()
-        pdf.add_page()
-        pdf.set_font("Helvetica", "B", 16)
-        pdf.multi_cell(0, 10, text=title)
-        pdf.ln(5)
-        pdf.set_font("Helvetica", "", 12)
-        pdf.multi_cell(0, 7, text=body)
+def anonymize_files():
+    for per_id, file in INPUT_DIR.glob("*.pdf"):
+        with pdfplumber.open(file) as pdf:
+            output_buffer = BytesIO()
 
-        pdf.output(str(OUTPUT_DIR/filename))
+            for page_num, page in enumerate(pdf.pages):
+                page_width, page_height = page.width, page.height
 
+                if page_num == 0:
+                    c = canvas.Canvas(output_buffer, page_width, page_height)
+                else:
+                    c.showPage()
 
-def read_pdfs():
-    for pdf in sorted(OUTPUT_DIR.glob("*.pdf")):
-        print(f"\n{pdf.name}")
-        for ln in PdfReader(pdf).pages[0].extract_text().splitlines()[:2]:
-            print(" ", ln.strip())
+                words = page.extract_words()
+
+                for word in words:
+                    text = word['text']
+                    anonymized_text = anonymize(text, per_id)
+
+                    c.setFont("Helvetica", 9)
+
+                    x = word['x0']
+                    y = page_height - word['top']
+
+                    c.drawString(x, y, anonymized_text)
+
+            c.save()
+
+            output_file = OUTPUT_DIR / file.name
+            with open(output_file, 'wb') as f:
+                f.write(output_buffer.getvalue())
 
 
 if __name__ == "__main__":
-    create_pdfs()
-    read_pdfs()
+    anonymize_files()
